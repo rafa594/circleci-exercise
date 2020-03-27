2c9a16f88996048480a583d6d79381c590dbdc83
rafa594 <rafa5941@gmail.com>
2020-03-27 13:48:54 -0600

    Using Environment Variables

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import json, os, time
 flag = False
 for i in range(10):
-    os.system("aws ecs describe-services --services rafa-service --cluster rafa-ecsECSCluster > service.json")
+    os.system("aws ecs describe-services --services "+ os.environ.get('SERVICE_NAME') + " --cluster "+ os.environ.get('CLUSTER_NAME') + " > service.json")
     text = open("service.json", "r").read()
     message = json.loads(text)["services"][0]["events"][0]["message"]
     if "steady" in message:
