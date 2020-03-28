a2fd05667999e45d41fc3a3ba3e703b925a04628
rafa594 <rafa5941@gmail.com>
2020-03-27 23:07:42 -0600

    Try with 2 instances

--- check.py
+++ check.py
@@ -1,16 +1,16 @@
 import json, os, time, sys
 flag = False
-time.sleep(3)
+
 for i in range(10):
+    time.sleep(60)
     os.system("aws ecs describe-services --services "+ os.environ.get('SERVICE_NAME') + " --cluster "+ os.environ.get('CLUSTER_NAME') + " > service.json")
     text = open("service.json", "r").read()
     message = json.loads(text)["services"][0]["events"][0]["message"]
     print(message)
     if "steady" in message:
         flag = True
-        break
-    else:
-        time.sleep(6)
+        break    
+        
 print (flag)
 if not flag:
     sys.exit(1)
