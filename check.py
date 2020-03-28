d85438103edd11d39e6e2e5397206c2e6c6c6372
rafa594 <rafa5941@gmail.com>
2020-03-27 21:55:42 -0600

    Using Environment Variables

--- check.py
+++ check.py
@@ -1,9 +1,11 @@
 import json, os, time
 flag = False
+time.sleep(30)
 for i in range(10):
     os.system("aws ecs describe-services --services "+ os.environ.get('SERVICE_NAME') + " --cluster "+ os.environ.get('CLUSTER_NAME') + " > service.json")
     text = open("service.json", "r").read()
     message = json.loads(text)["services"][0]["events"][0]["message"]
+    print(message)
     if "steady" in message:
         flag = True
         break
